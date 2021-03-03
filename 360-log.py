a7b3e75077339d47e7769eebc527527ab9f24def
Lucas Vinzon <lucasvinzon@gmail.com>
2021-03-03 16:39:20 -0300

    Create 360-log.py
    try

---
+++ 360-log.py
@@ -0,0 +1,18 @@
+import shutil, os, re
+
+path_root = "C:/user/name/Desktop/Kandao" #directory where you will store your folder which will contain the files
+#example "C:/user/name/Desktop/Kandao"
+directories = range(1,11) # number of photos, second param should be number of photos + 1. example: 10 photos
+#move all your photos to path_root. They should be inside no directory.
+
+#método
+def receive_files():
+  for dir in directories:
+    os.mkdirs(os.path.join(path_root, dir))
+  for file in os.listdir(path_root):
+    move = re.match('\d+{file_atm}_', file)
+    if (move):
+      shutil.move(move, f'{path_root}/{dir}')
+
+#run script
+receive_files()
